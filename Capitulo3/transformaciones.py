58ddbf8f5ca91f0bbbae6a11374f2a8dac06082f
NelsonStevenSM <nsanabiom@uni.pe>
2019-11-02 15:56:58 -0500

    Transformadores

--- Capitulo3/transformaciones.py
+++ Capitulo3/transformaciones.py
@@ -1,5 +1,17 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def powerLaw(img, gamma=0.4, c=1):
+
+    img = np.float32(img)
+    img_log = c*np.power(img,gamma)
+
+    funcG = img_log - min(img_log.flatten())
+    g_s = 255*(funcG/max(funcG.flatten()))
+
+    return np.uint8(g_s)
 
 def negative(img):
 
@@ -7,7 +19,7 @@ def negative(img):
 
 
 def logTransform(img,c=1):
-
+    img = np.float32(img)
     img_log = c*np.log(1+img)
 
     funcG = img_log - min(img_log.flatten())
@@ -15,17 +27,27 @@ def logTransform(img,c=1):
 
     return np.uint8(g_s)
 
-img = cv2.imread("./img/tomografia.png",0)
+img = cv2.imread("./img/ciudad.png",0)
 
 negatProc = negative(img)
 
 logtrans =logTransform(img, c=5)
+
+power = powerLaw(img, gamma = 3, c=4)
 
 cv2.imshow("Mamografia", img)
 
 cv2.imshow("NegativeRGB", negatProc)
 
 cv2.imshow("LogRGB", logtrans)
+
+cv2.imshow("Power", power)
+#
+#plt.plot(img.flatten(), logtrans.flatten())
+#plt.plot(img.flatten(), img.flatten())
+#plt.show()
+
+
 
 key = cv2.waitKey(0)
 
